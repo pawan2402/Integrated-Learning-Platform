50e7d65c4366fb3cd9fd46d0a9b93d6f2d84c745
pawan2402 <pawanshekhawat2402@gmail.com>
2024-04-29 22:16:09 +0530

    New Code Added

---
+++ assets/js/script.js
@@ -0,0 +1,26 @@
+function toggleNavDropdownCourses() {
+    var dropdownCourses = document.getElementById("navDropdownCourses");
+    if (dropdownCourses.style.display === "block") {
+        dropdownCourses.style.display = "none";
+    } else {
+        dropdownCourses.style.display = "block";
+    }
+}
+
+function toggleNavDropdownResources() {
+    var dropdownResources = document.getElementById("navDropdownResources");
+    if (dropdownResources.style.display === "block") {
+        dropdownResources.style.display = "none";
+    } else {
+        dropdownResources.style.display = "block";
+    }
+}
+
+function toggleNavDropdownLanguages() {
+    var dropdownLanguages = document.getElementById("navDropdownLanguages");
+    if (dropdownLanguages.style.display === "block") {
+        dropdownLanguages.style.display = "none";
+    } else {
+        dropdownLanguages.style.display = "block";
+    }
+} // Script For Navbar Dropdown
