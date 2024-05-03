48dc7f35261951a3e32bb9102671618274aa0e19
pawan2402 <pawanshekhawat2402@gmail.com>
2024-05-03 21:42:40 +0530

    Update

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,3 +24,44 @@ function toggleNavDropdownLanguages() {
         dropdownLanguages.style.display = "block";
     }
 } // Script For Navbar Dropdown
+
+
+var notyf = new Notyf();
+const submit = document.querySelector('.btn-sign');
+const email = document.querySelector('.email-btn');
+const password = document.querySelector('.password-btn');
+
+
+submit.addEventListener('click', (e) => {
+    e.preventDefault();
+    if (email.value.length > 2 && password.value.length > 2) {
+        notyf.success('please Wait...');
+        const ans = fetch('https://backend-integrated-learning-platform.onrender.com/login', {
+            method: 'POST',
+            mode: 'cors',
+            credentials: 'include',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*'
+            },
+            body: JSON.stringify({ email: email.value, password: password.value })
+        }).then(response => response.json())
+            .then(result => {
+                if (result.success) {
+                    notyf.success(result.message);
+                    localStorage.setItem('user', result.id);
+                    window.location.href = "./index.html";
+                }
+                else {
+                    notyf.error(result.message);
+                }
+            })
+    }
+    else {
+        notyf.error("email and password is too short");
+    }
+}); //script for handling form login submission
+
+
+
